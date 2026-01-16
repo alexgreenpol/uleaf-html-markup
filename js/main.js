a88b98032e982f50993bf65fa5076b51aefb54ef
Sukhrob Sanhinov <alexgreenpol@gmail.com>
2026-01-16 20:10:45 +0200

    Add changes to design

--- js/main.js
+++ js/main.js
@@ -1,52 +1,65 @@
-// BURGER 
-const burger = document.querySelector('.burger');
-const mobileMenu = document.querySelector('.mobile-menu');
+// BURGER
+const burger = document.querySelector(".burger");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-burger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('is-open');
-  burger.classList.toggle('is-active');
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("is-open");
+  burger.classList.toggle("is-active");
 });
 
 // IMAGE CAROUSEL
-const track = document.querySelector('.image-carousel__track');
+const track = document.querySelector(".image-carousel__track");
 
-let isDown = false;
-let startX;
-let scrollLeft;
+if (track) {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-track.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - track.offsetLeft;
-  scrollLeft = track.scrollLeft;
-});
+  track.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - track.offsetLeft;
+    scrollLeft = track.scrollLeft;
+  });
 
-track.addEventListener('mouseleave', () => {
-  isDown = false;
-});
+  track.addEventListener("mouseleave", () => {
+    isDown = false;
+  });
 
-track.addEventListener('mouseup', () => {
-  isDown = false;
-});
+  track.addEventListener("mouseup", () => {
+    isDown = false;
+  });
 
-track.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - track.offsetLeft;
-  const walk = (x - startX) * 1.2;
-  track.scrollLeft = scrollLeft - walk;
-});
+  track.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - track.offsetLeft;
+    const walk = (x - startX) * 1.2;
+    track.scrollLeft = scrollLeft - walk;
+  });
 
-let autoScroll = setInterval(() => {
-  track.scrollLeft += 1;
-}, 30);
-
-track.addEventListener('mouseenter', () => {
-  clearInterval(autoScroll);
-});
-
-track.addEventListener('mouseleave', () => {
-  autoScroll = setInterval(() => {
+  let autoScroll = setInterval(() => {
     track.scrollLeft += 1;
   }, 30);
-});
 
+  track.addEventListener("mouseenter", () => {
+    clearInterval(autoScroll);
+  });
+
+  track.addEventListener("mouseleave", () => {
+    autoScroll = setInterval(() => {
+      track.scrollLeft += 1;
+    }, 30);
+  });
+}
+
+// HEADER SCROLL
+
+const nav = document.querySelector(".site-nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    nav.classList.add("is-scrolled");
+  } else {
+    nav.classList.remove("is-scrolled");
+  }
+});
